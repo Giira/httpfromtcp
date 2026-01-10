d6e9094401a535df8f686d4fa0ff3b4a6704bc5e
Giira <euan.a.marsh@gmail.com>
2026-01-10 22:59:18 +0000

    Start udp version of the same package

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:42069")
+	if err != nil {
+		log.Fatalf("error resolving udp address: %v", err)
+	}
+	udpConn, err := net.DialUDP("udp", udpAddr, udpAddr)
+	if err != nil {
+		log.Fatalf("error dialing udp: %v", err)
+	}
+	defer udpConn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">")
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("errpr reading line: %v", err)
+		}
+		byteLine, err := json.Marshal(line)
+		if err != nil {
+			log.Fatalf("error marshalling line: %v", err)
+		}
+		_, err = udpConn.Write(byteLine)
+		if err != nil {
+			log.Fatalf("error writing line to udp connection: %v", err)
+		}
+	}
+}
