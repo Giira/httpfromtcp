97625bfcb62163426f8573b284ae7c8f94b1036a
Giira <euan.a.marsh@gmail.com>
2026-01-21 23:16:05 +0000

    edit tests more

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -35,7 +35,10 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
 	// Test: Invalid number of parts in request line
-	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	reader = &chunkReader{
+		data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 5,
+	}
 	require.Error(t, err)
 
 	// Test: Good POST Request with path
@@ -59,6 +62,13 @@ func TestRequestLineParse(t *testing.T) {
 	require.Error(t, err)
 
 	_, err = RequestFromReader(strings.NewReader("POST /cafe HTTP/1.2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: 0 byte reader
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 0,
+	}
 	require.Error(t, err)
 }
 
