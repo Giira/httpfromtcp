3823969a1c21aaf985fd5621dc1249a8fe5e0d04
Giira <euan.a.marsh@gmail.com>
2026-02-10 22:46:21 +0000

    Some more test skeletons

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -25,4 +25,22 @@ func Test_Headers_Parse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("            Host:      localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = NewHeaders()
+	data = []byte("")
+
+	// Test: Valid done
+	headers = NewHeaders()
+	data = []byte("")
 }
