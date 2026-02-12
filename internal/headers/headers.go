64bc92b0ce53f85defc86adb5059c07d7955b3ff
Giira <euan.a.marsh@gmail.com>
2026-02-12 22:58:29 +0000

    Start of header parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,7 +1,14 @@
 package headers
 
+import "strings"
+
 type Headers map[string]string
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	parts := strings.Split(string(data), "\r\n")
 
+}
+
+func NewHeaders() Headers {
+	return Headers{}
 }
