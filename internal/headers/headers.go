bf172b2693f7fc7431a2f0364a88d9172552a02a
Giira <euan.a.marsh@gmail.com>
2026-02-13 11:52:46 +0000

    handle start and end of data

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,11 +1,23 @@
 package headers
 
-import "strings"
+import (
+	"bytes"
+)
 
 type Headers map[string]string
 
+const crlf = "\r\n"
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	parts := strings.Split(string(data), "\r\n")
+	idx := bytes.Index(data, []byte(crlf))
+	switch idx {
+	case -1:
+		return 0, false, nil
+	case 0:
+		return 2, true, nil
+	default:
+		parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	}
 
 }
 
