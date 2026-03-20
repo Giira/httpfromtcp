275ca5387011947efab4813b37cad0c613f966cb
Giira <euan.a.marsh@gmail.com>
2026-03-20 23:08:04 +0000

    Update server handle func

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -32,23 +32,17 @@ func handler(w io.Writer, req *request.Request) server.HandlerError {
 	case "/yourproblem":
 		return server.HandlerError{
 			StatusCode: 400,
-			Message:    "<html>
-						<head>
-							<title>400 Bad Request</title>
-						</head>
-						<body>
-							<h1>Bad Request</h1>
-							<p>Your request honestly kinda sucked.</p>
-						</body>
-						</html>",
+			Message:    "<html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your request honestly kinda sucked.</p></body></html>",
 		}
 	case "/myproblem":
 		return server.HandlerError{
 			StatusCode: 500,
-			Message:    "Woopsie, my bad\n",
+			Message:    "<html><head><title>500 Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>Okay, you know what? This one is on me.</p></body></html>",
 		}
 	default:
-		w.Write([]byte("All good, frfr\n"))
-		return server.HandlerError{}
+		return server.HandlerError{
+			StatusCode: 200,
+			Message:    "<html><head><title>200 OK</title></head><body><h1>Success!</h1><p>Your request was an absolute banger.</p></body></html>",
+		}
 	}
 }
