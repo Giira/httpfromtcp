472bdd84bf0f91a7891341836d17635250978438
Giira <euan.a.marsh@gmail.com>
2026-03-22 22:49:54 +0000

    response refactor to fix previous errors and make legible - up to body

--- internal/response/response.go
+++ internal/response/response.go
@@ -17,32 +17,50 @@ const (
 type WriterState int
 
 const (
-	WriteSL WriterState = iota
-	WriteH
-	WriteB
+	WriterInitialised WriterState = iota
+	WriterStatusLineDone
+	WriterHeadersDone
+	WriterBodyDone
 )
 
 type Writer struct {
-	State  WriterState
-	Writer io.Writer
+	state WriterState
+	conn  io.Writer
+}
+
+func NewWriter(conn io.Writer) *Writer {
+	return &Writer{
+		conn:  conn,
+		state: WriterInitialised,
+	}
 }
 
 func (w *Writer) WriteStatusLine(statusCode StatusCode) error {
-	var err error
+	if w.state != WriterInitialised {
+		return fmt.Errorf("error: invalid state for writing statusline: %v", w.state)
+	}
+
+	var reasonString string
+
 	switch statusCode {
 	case 200:
-		_, err = w.Writer.Write([]byte("HTTP/1.1 200 OK"))
+		reasonString = "OK"
 	case 400:
-		_, err = w.Writer.Write([]byte("HTTP/1.1 400 Bad Request"))
+		reasonString = "Bad Request"
 	case 500:
-		_, err = w.Writer.Write([]byte("HTTP/1.1 500 Internal Server Error"))
+		reasonString = "Internal Server Error"
 	default:
-		out := fmt.Sprintf("HTTP/1.1 %v", statusCode)
-		_, err = w.Writer.Write([]byte(out))
+		reasonString = ""
 	}
+
+	_, err := io.WriteString(w.conn, fmt.Sprintf("HTTP/1.1 %d %s\r\n", statusCode, reasonString))
+
 	if err != nil {
 		return fmt.Errorf("error: failed to write statusline: %v", err)
 	}
+
+	w.state = WriterStatusLineDone
+
 	return nil
 }
 
@@ -55,17 +73,27 @@ func GetDefaultHeaders(contentLen int) headers.Headers {
 }
 
 func (w *Writer) WriteHeaders(headers headers.Headers) error {
+	if w.state != WriterStatusLineDone {
+		return fmt.Errorf("error: invalid state for writing headers: %v", w.state)
+	}
+
 	out := []byte{}
 	for k, v := range headers {
 		out = fmt.Appendf(out, "%v: %v\r\n", k, v)
 	}
 	out = append(out, []byte("\r\n")...)
-	_, err := w.Writer.Write(out)
-	return err
+	_, err := w.conn.Write(out)
+	if err != nil {
+		return fmt.Errorf("error: failed to write headers to connection: %v", err)
+	}
+
+	w.state = WriterHeadersDone
+
+	return nil
 }
 
 func (w *Writer) WriteBody(p []byte) (int, error) {
-	if w.State != WriteB {
-		return 0, fmt.Errorf("error: incorrect writer state for body writing: %v", w.State)
+	if w.state != WriterHeadersDone {
+		return 0, fmt.Errorf("error: incorrect writer state for body writing: %v", w.state)
 	}
 }
