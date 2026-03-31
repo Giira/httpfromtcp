5377963adc7f7981cc43b49eeca28399b0b670c0
Giira <euan.a.marsh@gmail.com>
2026-03-31 23:19:48 +0100

    httpserver handler func now functions properly

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -28,21 +28,20 @@ func main() {
 
 func handler(w *response.Writer, req *request.Request) {
 	target := req.RequestLine.RequestTarget
+	var body []byte
 	switch target {
 	case "/yourproblem":
-		return server.HandlerError{
-			StatusCode: 400,
-			Message:    "<html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your request honestly kinda sucked.</p></body></html>",
-		}
+		w.WriteStatusLine(response.CodeBadRequest)
+		body = []byte("<html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your request honestly kinda sucked.</p></body></html>")
 	case "/myproblem":
-		return server.HandlerError{
-			StatusCode: 500,
-			Message:    "<html><head><title>500 Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>Okay, you know what? This one is on me.</p></body></html>",
-		}
+		w.WriteStatusLine(response.CodeServerError)
+		body = []byte("<html><head><title>500 Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>Okay, you know what? This one is on me.</p></body></html>")
 	default:
-		return server.HandlerError{
-			StatusCode: 200,
-			Message:    "<html><head><title>200 OK</title></head><body><h1>Success!</h1><p>Your request was an absolute banger.</p></body></html>",
-		}
+		w.WriteStatusLine(response.CodeOK)
+		body = []byte("<html><head><title>200 OK</title></head><body><h1>Success!</h1><p>Your request was an absolute banger.</p></body></html>")
 	}
+	h := response.GetDefaultHeaders(len(body))
+	h.Change("Content-Type", "text/html")
+	w.WriteHeaders(h)
+	w.WriteBody(body)
 }
